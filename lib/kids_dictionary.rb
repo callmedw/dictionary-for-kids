bf4b6e65ba7360cb9baa6d142ecd8c2d2bb3510e
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-28 09:32:11 -0700

    add method and passing test for id of Word class

--- lib/kids_dictionary.rb
+++ lib/kids_dictionary.rb
@@ -1,10 +1,15 @@
 require 'ruby-lint'
 
 class Word
-  attr_reader(:term)
+  attr_reader(:term, :id)
+
+  @@words = []
 
   define_method(:initialize) do |attributes|
     @term = attributes.fetch(:term)
+    @id = @@words.length + 1
 
   end
+
+
 end
--- spec/kids_dictionary_spec.rb
+++ spec/kids_dictionary_spec.rb
@@ -8,8 +8,18 @@ describe(Word) do
 
   describe('#term') do
     it('returns the term') do
-      test_word = Word.new({:term => 'apple'})
+      test_word = Word.new({:term => 'apple', :id => 1})
       expect(test_word.term()).to(eq('apple'))
     end
   end
+
+describe('#id') do
+   it('returns the ID of the term') do
+     test_word = Word.new({:term => 'apple', :id => 1})
+     expect(test_word.id()).to(eq(1))
+   end
+ end
+
+
+
 end
